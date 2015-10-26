0f1a6d595070f176bbf8eb336a570f8a38bc5182
Jonathan <jt21091@gmail.com>
2015-10-26 15:51:38 -0500

    further readied for production

--- basicblog/settings/base.py
+++ basicblog/settings/base.py
@@ -104,7 +104,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
